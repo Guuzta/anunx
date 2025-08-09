ce8e307e44b4514b0ecf539c71bbb314e6c7b2e0
Guuzta <Guuhsilva13@outlook.com>
2025-08-09 19:01:53 -0300

    adicionando template default

---
+++ src/templates/Default.js
@@ -0,0 +1,14 @@
+'use client'
+
+import Header from '../components/Header'
+
+const Default = ({ children }) => {
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    )
+}
+
+export default Default
