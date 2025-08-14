1fa5efb6b8eb259e0c9fa399be315682d68766a3
Guuzta <Guuhsilva13@outlook.com>
2025-08-14 16:29:55 -0300

    adicionando home deslogada

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,115 @@
-export default function Dashboard() {
+'use client'
+
+import {
+  Card,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Button,
+  Paper,
+  Container,
+  IconButton,
+  InputBase,
+  Typography,
+  Grid
+} from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search'
+
+export default function Home() {
   return (
-    <div>Login</div>
+    <>
+      <Container maxWidth='md' sx={{ paddingTop: '45px', width: '60%' }}>
+        <Typography component='h1' variant='h4' align='center'>
+          O que deseja encontrar?
+        </Typography>
+        <Paper sx={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
+          <InputBase
+            placeholder="Ex.: Escreva um produto que deseja encontrar"
+            sx={{ padding: '5px 10px' }}
+            fullWidth
+          ></InputBase>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </Container>
+
+      <Container maxWidth='md' sx={{ paddingTop: '45px' }}>
+        <Typography component='h2' variant='h4' align='center' gutterBottom>
+          Destaques
+        </Typography>
+
+        <Grid container spacing={4}>
+          <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+            <Card sx={{ maxWidth: 300 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://i.ytimg.com/vi/oh5YvNgw7o8/maxresdefault.jpg"
+                title="random image"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Produto
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  R$ 30,90
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+            <Card sx={{ maxWidth: 300 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://i.ytimg.com/vi/oh5YvNgw7o8/maxresdefault.jpg"
+                title="random image"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Produto
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  R$ 30,90
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+            <Card sx={{ maxWidth: 300 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://i.ytimg.com/vi/oh5YvNgw7o8/maxresdefault.jpg"
+                title="random image"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Produto
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  R$ 30,90
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+            <Card sx={{ maxWidth: 300 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://i.ytimg.com/vi/oh5YvNgw7o8/maxresdefault.jpg"
+                title="random image"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Produto
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  R$ 30,90
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   )
 }
