5e02b36dd5cdfd7e45416d2a1b6ea56595a0a817
Guuzta <Guuhsilva13@outlook.com>
2025-09-06 14:07:54 -0300

    modificando componente toasy - adicionando funcao para fechar automaticamente

--- src/components/Toasty.js
+++ src/components/Toasty.js
@@ -1,25 +1,28 @@
-import * as React from 'react'
+import { useState, useEffect } from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
 
 const Toasty = ({ open, text, severity, onClose = null }) => {
 
-   
+    const [openToasty, setOpenToasty] = useState(open)
+    
+    useEffect(() => {
+        setOpenToasty(open)
+    }, [open])
+
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return
         }
 
-
-    }
+        setOpenToasty(false)   
+    }  
 
     if (onClose) onClose()
-
+    
     return (
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={openToasty} autoHideDuration={3000} onClose={handleClose}>
             <Alert
-                onClose={handleClose}
                 severity={severity}
                 variant="filled"
                 sx={{ width: '100%' }}
